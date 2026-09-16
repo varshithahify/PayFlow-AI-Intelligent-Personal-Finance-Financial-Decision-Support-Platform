88f61cd573c00c56b90ffe6b7f9d4ba4f2aca5bb
varshithahify <varshithavn26@gmail.com>
2026-09-16 10:17:46 +0530

    Add authentication service

--- backend/payflow-backend/src/main/java/com/payflow/payflow_backend/service/AuthService.java
+++ backend/payflow-backend/src/main/java/com/payflow/payflow_backend/service/AuthService.java
@@ -1,8 +1,10 @@
 package com.payflow.payflow_backend.service;
 
+import com.payflow.payflow_backend.dto.LoginRequest;
+import com.payflow.payflow_backend.dto.RegisterRequest;
 import com.payflow.payflow_backend.entity.User;
 import com.payflow.payflow_backend.repository.UserRepository;
-
+import com.payflow.payflow_backend.security.JwtService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -16,23 +18,48 @@ public class AuthService {
     public AuthService(
             UserRepository userRepository,
             PasswordEncoder passwordEncoder,
-            JwtService jwtService) {
-
+            JwtService jwtService
+    ) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
         this.jwtService = jwtService;
     }
 
-    public String login(String email, String password) {
+    public User register(RegisterRequest request) {
 
-        User user = userRepository.findByEmail(email);
+        User existingUser = userRepository.findByEmail(request.getEmail());
 
-        if (user == null) {
-            throw new RuntimeException("Invalid email or password");
+        if (existingUser != null) {
+            throw new IllegalArgumentException("Email already registered");
         }
 
-        if (!passwordEncoder.matches(password, user.getPasswordHash())) {
-            throw new RuntimeException("Invalid email or password");
+        String hashedPassword =
+                passwordEncoder.encode(request.getPassword());
+
+        User user = new User(
+                request.getName(),
+                request.getEmail(),
+                hashedPassword
+        );
+
+        return userRepository.save(user);
+    }
+
+    public String login(LoginRequest request) {
+
+        User user = userRepository.findByEmail(request.getEmail());
+
+        if (user == null) {
+            throw new IllegalArgumentException("Invalid email or password");
+        }
+
+        boolean passwordMatches = passwordEncoder.matches(
+                request.getPassword(),
+                user.getPasswordHash()
+        );
+
+        if (!passwordMatches) {
+            throw new IllegalArgumentException("Invalid email or password");
         }
 
         return jwtService.generateToken(user.getEmail());
