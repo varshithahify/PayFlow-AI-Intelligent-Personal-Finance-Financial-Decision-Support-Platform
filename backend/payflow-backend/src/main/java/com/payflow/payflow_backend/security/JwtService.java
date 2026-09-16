e0a2cf1c58d6b6333be362e9eb0c21196af5198a
varshithahify <varshithavn26@gmail.com>
2026-09-16 10:18:29 +0530

    Add JWT service

---
+++ backend/payflow-backend/src/main/java/com/payflow/payflow_backend/security/JwtService.java
@@ -0,0 +1,62 @@
+package com.payflow.payflow_backend.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Service
+public class JwtService {
+
+    private final SecretKey secretKey;
+    private final long expirationTime;
+
+    public JwtService(
+            @Value("${jwt.secret}") String secret,
+            @Value("${jwt.expiration}") long expirationTime
+    ) {
+        this.secretKey = Keys.hmacShaKeyFor(
+                secret.getBytes(StandardCharsets.UTF_8)
+        );
+        this.expirationTime = expirationTime;
+    }
+
+    public String generateToken(String email) {
+
+        Date now = new Date();
+        Date expiration = new Date(
+                now.getTime() + expirationTime
+        );
+
+        return Jwts.builder()
+                .subject(email)
+                .issuedAt(now)
+                .expiration(expiration)
+                .signWith(secretKey)
+                .compact();
+    }
+
+    public String extractEmail(String token) {
+
+        return Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
+    }
+
+    public boolean isTokenValid(String token) {
+
+        try {
+            extractEmail(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
