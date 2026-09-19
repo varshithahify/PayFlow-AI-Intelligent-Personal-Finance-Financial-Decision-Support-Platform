39727ccd1ac0dd7d31f0828b2d01c65980cfb1cd
varshithahify <varshithavn26@gmail.com>
2026-09-19 23:20:51 +0530

    Add payment gateway router

---
+++ backend/payflow-backend/src/main/java/com/payflow/payflow_backend/gateway/GatewayRouter.java
@@ -0,0 +1,33 @@
+package com.payflow.payflow_backend.gateway;
+
+import com.payflow.payflow_backend.entity.Transaction;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class GatewayRouter {
+
+    private final List<PaymentGateway> gateways;
+
+    public GatewayRouter(List<PaymentGateway> gateways) {
+        this.gateways = gateways;
+    }
+
+    public PaymentGateway route(Transaction transaction) {
+
+        return gateways.stream()
+                .filter(gateway ->
+                        gateway.supports(
+                                transaction.getPaymentMethod()
+                        )
+                )
+                .findFirst()
+                .orElseThrow(() ->
+                        new IllegalArgumentException(
+                                "No payment gateway supports payment method: "
+                                        + transaction.getPaymentMethod()
+                        )
+                );
+    }
+}
