b8285ecc659943023141891d3ee7a603352337f7
varshithahify <varshithavn26@gmail.com>
2026-09-20 12:40:54 +0530

    Add investigation repository

---
+++ backend/payflow-backend/src/main/java/com/payflow/payflow_backend/repository/InvestigationRepository.java
@@ -0,0 +1,26 @@
+package com.payflow.payflow_backend.repository;
+
+import com.payflow.payflow_backend.entity.Investigation;
+import com.payflow.payflow_backend.entity.InvestigationPriority;
+import com.payflow.payflow_backend.entity.InvestigationStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface InvestigationRepository
+        extends JpaRepository<Investigation, Long> {
+
+    List<Investigation> findByStatusOrderByCreatedAtDesc(
+            InvestigationStatus status);
+
+    List<Investigation> findByPriorityOrderByCreatedAtDesc(
+            InvestigationPriority priority);
+
+    List<Investigation> findByTransactionIdOrderByCreatedAtDesc(
+            Long transactionId);
+
+    Optional<Investigation>
+    findTopByTransactionIdOrderByCreatedAtDesc(
+            Long transactionId);
+}
