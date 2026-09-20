50482d459e29e54f1b9ed0a6c07392ef4e3c3d9e
varshithahify <varshithavn26@gmail.com>
2026-09-20 12:32:01 +0530

    Add investigation issue types

---
+++ backend/payflow-backend/src/main/java/com/payflow/payflow_backend/entity/InvestigationIssueType.java
@@ -0,0 +1,14 @@
+package com.payflow.payflow_backend.entity;
+
+public enum InvestigationIssueType {
+
+    AMOUNT_MISMATCH,
+
+    CURRENCY_MISMATCH,
+
+    STATUS_MISMATCH,
+
+    MISSING_EXTERNAL,
+
+    MISSING_INTERNAL
+}
