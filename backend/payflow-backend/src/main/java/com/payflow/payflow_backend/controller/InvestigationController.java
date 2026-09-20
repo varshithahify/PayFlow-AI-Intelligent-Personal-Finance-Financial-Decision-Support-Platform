bbc613bd654c0c35361a622ed176fa2b4dbdda8c
varshithahify <varshithavn26@gmail.com>
2026-09-20 15:36:05 +0530

    Add investigation REST endpoints

--- backend/payflow-backend/src/main/java/com/payflow/payflow_backend/controller/InvestigationController.java
+++ backend/payflow-backend/src/main/java/com/payflow/payflow_backend/controller/InvestigationController.java
@@ -19,19 +19,23 @@ public class InvestigationController {
     public InvestigationController(
             InvestigationService investigationService) {
 
-        this.investigationService =
-                investigationService;
+        this.investigationService = investigationService;
     }
 
     @PostMapping
     public ResponseEntity<Investigation> createInvestigation(
-            @RequestParam Long transactionId,
+            @RequestParam(required = false)
+            Long transactionId,
+
             @RequestParam(required = false)
             Long reconciliationRecordId,
+
             @RequestParam
             InvestigationPriority priority,
+
             @RequestParam
             InvestigationIssueType issueType,
+
             @RequestParam
             String summary) {
 
